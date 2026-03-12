c8b6ab5e92aa4d3689afdc6351087c777539c77f
hyun7931 <shlee4185@gmail.com>
2026-03-12 10:57:03 +0900

    feat: add Entity

--- src/main/java/dev/authorization/model/Authority.java
+++ src/main/java/dev/authorization/model/Authority.java
@@ -1,4 +1,28 @@
 package dev.authorization.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // 권한 (user / admin)
+    private String authority;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    protected Authority() {}
+
+    public Authority(String authority) {
+        this.authority = authority;
+    }
 }
--- src/main/java/dev/authorization/model/User.java
+++ src/main/java/dev/authorization/model/User.java
@@ -1,4 +1,48 @@
 package dev.authorization.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String position;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Authority> authorities = new ArrayList<>();
+
+    protected User() {}
+
+    public User(String username, String password, String position) {
+        this.username = username;
+        this.password = password;
+        this.position = position;
+    }
+
+    // 연관관계 편의 메서드
+    public void addAuthority(Authority authority) {
+        authorities.add(authority);
+        authority.setUser(this);
+    }
+
+    public void encodePassword(String encodedPassword) {
+        this.password = encodedPassword;
+    }
 }

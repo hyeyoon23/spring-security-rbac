5cc73455f243c4985ce7ff66514e92c606558cde
hyun7931 <shlee4185@gmail.com>
2026-03-12 12:00:25 +0900

    feat: add AdminController

--- src/main/java/dev/authorization/controller/AdminController.java
+++ src/main/java/dev/authorization/controller/AdminController.java
@@ -1,4 +1,27 @@
 package dev.authorization.controller;
 
+import dev.authorization.model.User;
+import dev.authorization.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final UserService userService;
+
+    @GetMapping("/adminpage")
+    public String adminPage(Model model) {
+
+        List<User> users = userService.getAllUsers();
+
+        model.addAttribute("users", users);
+
+        return "adminpage";
+    }
 }
